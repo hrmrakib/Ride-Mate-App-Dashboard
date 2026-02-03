4ecb90113674553a710a04451d2defa9ac12ee8f
Md Rakibul Alam <hrmrakibs@gmail.com>
2026-02-03 09:36:04 +0600

    fix: update hostname for remote image pattern in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "api.getavails.com",
+        hostname: "api.radeefz.com",
       },
       {
         protocol: "http",
